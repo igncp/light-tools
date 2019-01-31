6363fb5edcd61c541694fac33d5537e5953c009d
Ignacio Carbajo <icarbajop@gmail.com>
2019-01-31 23:23:23 +0800

    Start building tree

--- tools/o/src/main.rs
+++ tools/o/src/main.rs
@@ -2,6 +2,7 @@
 extern crate serde_derive;
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::remove_file;
 use std::fs::DirBuilder;
 use std::fs::File;
@@ -188,10 +189,18 @@ fn handle_csv(matches: &ArgMatches<'_>) {
   }
 }
 
+#[derive(Debug, Clone)]
+struct TreeNode {
+  children: HashSet<usize>,
+  parent: Option<usize>
+}
+
+#[derive(Debug, Clone)]
 struct Context {
   id_to_str_map: HashMap<usize, String>,
   str_to_id_map: HashMap<String, usize>,
   id_to_record_idx_map: HashMap<usize, usize>,
+  hierarchy: HashMap<usize, TreeNode>,
   max_id: usize,
 }
 
@@ -199,14 +208,26 @@ fn get_context(records: &[Record]) -> Context {
   let mut str_to_id_map: HashMap<String, usize> = HashMap::new();
   let mut id_to_str_map: HashMap<usize, String> = HashMap::new();
   let mut id_to_record_idx_map: HashMap<usize, usize> = HashMap::new();
+  let mut hierarchy: HashMap<usize, TreeNode> = HashMap::new();
   let mut max_id = 0;
 
   for (idx, record) in records.iter().enumerate() {
+    let empty_branch = TreeNode {
+      parent: None,
+      children: HashSet::new()
+    };
+
     str_to_id_map.insert(record.what.clone(), record.what_id);
     str_to_id_map.insert(record.location.clone(), record.location_id);
 
     id_to_str_map.insert(record.what_id, record.what.clone());
     id_to_str_map.insert(record.location_id, record.location.clone());
+
+    let what_branch = hierarchy.entry(record.what_id).or_insert(empty_branch.clone());
+    what_branch.parent = Some(record.location_id);
+
+    let location_branch = hierarchy.entry(record.location_id).or_insert(empty_branch.clone());
+    location_branch.children.insert(record.what_id);
 
     max_id = std::cmp::max(max_id, record.location_id);
     max_id = std::cmp::max(max_id, record.what_id);
@@ -218,6 +239,7 @@ fn get_context(records: &[Record]) -> Context {
     str_to_id_map,
     id_to_str_map,
     id_to_record_idx_map,
+    hierarchy,
     max_id,
   }
 }
@@ -417,9 +439,18 @@ fn handle_remove(matches: &ArgMatches<'_>) {
 
 fn handle_stats() {
   let records = get_data_records();
+  let context = get_context(&records);
+  let mut root_nodes_num = 0;
+
+  for id in context.hierarchy.keys() {
+    if context.hierarchy[id].parent.is_none() {
+      root_nodes_num += 1;
+    }
+  }
 
   println!("Stats:");
   println!("- Count: {}", records.len());
+  println!("- Root nodes: {}", root_nodes_num);
 }
 
 fn handle_list() {
